0d6fae974ba47e626d0298bbb0d83407844cb605
Denil Job <83473403+Deniljob@users.noreply.github.com>
2024-02-13 21:56:40 +0530

    Update Login.jsx for production

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import "./Login.css";
 import axios from "axios";
 
 const URL =
-  "https://accounts.spotify.com/authorize?client_id=3013bba738be4ae2ac755bec41b88e29&response_type=code&redirect_uri=http://localhost:5173/&scope=user-read-private%20user-read-email%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private";
+  "https://accounts.spotify.com/authorize?client_id=3013bba738be4ae2ac755bec41b88e29&response_type=code&redirect_uri=https://spotify-playlist-server.onrender.com/&scope=user-read-private%20user-read-email%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private";
 
 function Login() {
   useEffect(() => {
